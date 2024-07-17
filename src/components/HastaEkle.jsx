c7ea0034dc8a588252a32ff94e26113f84db7aaf
ada <h.kaansolmaz@gmail.com>
2024-07-17 11:40:26 +0300

    b

--- src/components/HastaEkle.jsx
+++ src/components/HastaEkle.jsx
@@ -1,14 +1,27 @@
 import React, { useState } from "react";
+import uuid from "react-uuid";
 
-const HastaEkle = ({hastalar, setHastalar}) => {
+const HastaEkle = ({ hastalar, setHastalar }) => {
     const [hastaName, setHastaName] = useState("");
     const [hastaTarih, setTarih] = useState("");
-    
-    const handleSubmit=(e)=>{
-      e.preventDefault() //direkt submit yapma once alttaki kodlara bak
 
-
-    }
+    const handleSubmit = (e) => {
+        e.preventDefault(); //direkt submit yapma once alttaki kodlara bak
+        setHastalar([
+            ...hastalar,
+            {
+                id: uuid(),
+                text: hastaName,
+                day: hastaTarih,
+                isDone: false,
+                myDoctor: "DR Ipek Bilir",
+            },
+        ]);
+        // setHastalar a gidildiğinde hemen altta console.log varsa çalışıverir ve biz useState nin yaptığı işi göremeyiz. bunun için log bu submit fonksiyonunun dışında olmalı.
+        setHastaName("")
+        setTarih("")
+        // submit sonrası inputlardan value temizlemek için, hem alttaki işlemler yapılır, hemde inputlarda value={isim} yazarak browser da boşluksa boşluk isimse isim gözükmesi sağlanır
+    };
 
     return (
         <div>
