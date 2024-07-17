aec4c8637c0af0274a038a91c51c9c02ace86f17
ada <h.kaansolmaz@gmail.com>
2024-07-17 12:34:35 +0300

    ff

--- src/components/HastaListe.jsx
+++ src/components/HastaListe.jsx
@@ -5,20 +5,21 @@ const HastaListe = ({ hastalar, setHastalar, doktorlar }) => {
     <div>
       {hastalar.map((hasta) => (
         <div key={hasta.id}>
-          <div>
-            <div>
-<h2>{hasta.text} </h2>
-<h4>{hasta.day}</h4>
-<h3>{hasta.myDoctor}</h3>
+          <div className={hasta.isDone ? "trueStil":"falseStyle"}
+onDoubleClick={()=>setHastalar(hastalar.map((a)=>a.id===hasta.id ? {...a,isDone:!a.isDone}:a))}
 
+          >
+            <div>
+              <h2>{hasta.text} </h2>
+              <h4>{hasta.day}</h4>
+              <h3>{hasta.myDoctor}</h3>
             </div>
             <div>
-              <FaTimesCircle style={{color:"red"}}/>
+              <FaTimesCircle style={{ color: "red" }} />
             </div>
           </div>
         </div>
       ))}
-
     </div>
   );
 };
