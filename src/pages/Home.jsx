ed2e3282c844a2cad6b3b4b80d2a41e1792c267a
ada <h.kaansolmaz@gmail.com>
2024-07-17 10:33:30 +0300

    home-d

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useState } from "react";
+
+import { doctorData } from "../helper/Data";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+    const [doktorlar, setDoktorlar] = useState(doctorData);
 
-export default Home
+    return (
+        <div>
+            <div>
+                <header className="header">
+                    <h1>HOSPITAL</h1>
+                    <div className="dr">
+                        {doctorData.map((doc) => (
+                            <div>
+                                <img
+                                    width="180px"
+                                    height="150px"
+                                    className="btn"
+                                    style={{backgroundColor:"aqua"}}
+                                    src={doc.doctorImg}
+                                    alt=""
+                                />
+                                <h4 style={{backgroundColor:"aqua", borderLeft:"10px solid blue"}}>{doc.doctorName}</h4>
+                            </div>
+                        ))}
+                    </div>
+                </header>
+                <div>{/* hasta ekle */}</div>
+                {/* hasta liste */}
+            </div>
+        </div>
+    );
+};
+
+export default Home;
